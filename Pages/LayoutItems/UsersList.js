155e08288342dccd68fcfac0f1efc9599863981b
Harrison Miller <harrisonmiller@Harrisons-MacBook-Pro.local>
2015-10-24 17:13:13 -0400

    Speed up users list initial delay for filter loading

--- Pages/LayoutItems/UsersList.js
+++ Pages/LayoutItems/UsersList.js
@@ -487,7 +487,7 @@ var UsersList = React.createClass({
                     _this.setState({currentUserData: snapshot.val(), showCurrentUser: true});
                 });
 
-            }, 500);
+            }, 200);
         });
     },
 
